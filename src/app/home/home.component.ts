a5bd22f3d6d64f94fd0c02cefdfad816f5358eb9
Nathan Pickard <nathan.pickard87@gmail.com>
2019-06-22 21:41:22 -0700

    Custom interval observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
-import { Observer, Subscription } from 'rxjs/Rx';
-import { interval } from 'rxjs';
+import { Observable, Observer, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -22,22 +19,34 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-    this.firstObsSubscription = interval(period: 1000).subscribe(next: count => {
-      console.log(count);
-    })
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
 
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
 
-    const myNumbers = Observable.interval(1000)
-      .map(
-        (data: number) => {
-          return data * 2;
-        }
-      );
-    this.numbersObsSubscription = myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
-      }
-    );
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    });
+
+    // const myNumbers = Observable.interval(1000)
+    //   .map(
+    //     (data: number) => {
+    //       return data * 2;
+    //     }
+    //   );
+    // this.numbersObsSubscription = myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
 
     const myObservable = Observable.create((observer: Observer<string>) => {
       setTimeout(() => {
